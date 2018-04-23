1bdb351a5a31d3328b4ecffe050724b22f04f657
HaiDunia <32042279+HaiDunia@users.noreply.github.com>
2018-04-22 23:11:04 -0500

    Update TableRowRewriter.py

--- TableRowRewriter.py
+++ TableRowRewriter.py
@@ -1,19 +1,67 @@
-
 #recomend repl.it
-dayOfMonth = 25
-#change the range to match the next row 
-#row 1 (0,6)
-#row 2 (7,13)
-#so on
-for indexs in range(0,6):
-  sindexs = str(indexs)
-  #using a+= to get rid of the space before the inputed numbers
-  a= ("<td onclick=\"colorPrompt(this,")
-  a+=sindexs
-  a+=(")\"><div class=\"day\">")
-  a+=str(dayOfMonth)
-  a+=("</div><div id=\"")
-  a+=sindexs
-  a+=("\"> </div></td>")
-  print(a)
-  dayOfMonth+=1
+#1.2
+#hides tables by default
+import datetime
+idNum = 0
+for i in range(0,12):
+  print("<div class=\"DayRow\" style=\"display:none;\">")
+  print("  <table>")
+  print("    <tbody>")
+  print("      <tr>")
+  
+  
+  
+  leap = (4==(datetime.date.today().year%4))
+  currentMonth = i
+  
+  if(leap):
+    months= [31,29,31,30,31,30,31,31,30,31,30,31]
+  else:
+    months= [31,28,31,30,31,30,31,31,30,31,30,31]
+  
+  if(currentMonth == 0):
+    endOfMonth = months[11]
+  else:
+    endOfMonth = months[currentMonth-1]
+  newMonth = months[currentMonth]
+  dayOfMonthH = endOfMonth - (datetime.date(2018, currentMonth+1, 1).isoweekday()-1)
+
+  count = -1
+  print("        <tr>")
+  
+  for indexs in range(0,42):
+    if(count == 6):
+      print("      </tr>")
+      print("      <tr>")
+      count = 0
+    else:
+      count +=1
+    sindexs = str(indexs)
+    sid = str(idNum)
+    if(dayOfMonthH >endOfMonth):
+      modifiedDate = dayOfMonthH-endOfMonth
+      if(modifiedDate>newMonth):
+        dayOfMonth= modifiedDate - newMonth
+      else:
+        dayOfMonth= modifiedDate
+    else:
+      dayOfMonth= dayOfMonthH
+    #using a+= to get rid of the space before the inputed numbers
+    a= ("        <td onclick=\"colorPrompt(this,")
+    a+=sid
+    a+=(")\"><div class=\"day\">")
+    a+=str(dayOfMonth)
+    a+=("</div><div id=\"")
+    a+=sid
+    a+=("\"> </div></td>")
+    print(a)
+    dayOfMonthH+=1
+    idNum+=1
+  
+  print("      </tr>")
+  print("    </tbody>")
+  print("  </table>")
+  print("</div>")
+  print("")
+  print("")
+  idNum+=-14
